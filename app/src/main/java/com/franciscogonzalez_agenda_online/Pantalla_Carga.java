5c72e16ace0e23afcd11b01d18a7b4721a14bc1a
Francisco <gonzalezfranl10@gmail.com>
2022-03-10 11:16:44 -0300

    Creacion y primer commit

---
+++ app/src/main/java/com/franciscogonzalez_agenda_online/Pantalla_Carga.java
@@ -0,0 +1,14 @@
+package com.franciscogonzalez_agenda_online;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pantalla_Carga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_carga);
+    }
+}
