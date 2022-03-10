be4003841f51d3082211eaaccc0bec1d03749aba
Francisco <gonzalezfranl10@gmail.com>
2022-03-10 15:43:02 -0300

    Primeras layouts, principio de botones de login y registro

--- app/src/main/java/com/franciscogonzalez_agenda_online/MainActivity.java
+++ app/src/main/java/com/franciscogonzalez_agenda_online/MainActivity.java
@@ -3,12 +3,18 @@ package com.franciscogonzalez_agenda_online;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_login , btn_registro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_login = findViewById(R.id.boton_login);
+        btn_registro = findViewById(R.id.boton_registro);
     }
 }
--- app/src/main/java/com/franciscogonzalez_agenda_online/Pantalla_Carga.java
+++ app/src/main/java/com/franciscogonzalez_agenda_online/Pantalla_Carga.java
@@ -2,7 +2,9 @@ package com.franciscogonzalez_agenda_online;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Pantalla_Carga extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class Pantalla_Carga extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_carga);
+
+        int tiempo = 3000;   // 3 segundos
+
+        // El objeto Handler permite ejecutar lineas de codigo en el metodo run() despues de un tiempo determinado
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Pantalla_Carga.this, MainActivity.class));
+                finish();    //se destruye al llegar al MainActivity
+            }
+        },tiempo);
     }
 }
