109c27ddea024fd1d388ef5f8e6b64e496f2cd19
Francisco <gonzalezfranl10@gmail.com>
2022-03-11 12:09:11 -0300

    Interfaz registro

--- app/src/main/java/com/franciscogonzalez_agenda_online/MainActivity.java
+++ app/src/main/java/com/franciscogonzalez_agenda_online/MainActivity.java
@@ -2,7 +2,9 @@ package com.franciscogonzalez_agenda_online;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,7 +16,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Inicializacion botones
         btn_login = findViewById(R.id.boton_login);
         btn_registro = findViewById(R.id.boton_registro);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Login.class));      //Al clickear nos dirige de MainActivity a la actividad Login.class
+            }
+        });
+
+        btn_registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Registro.class));      //Al clickear nos dirige de MainActivity a la actividad Registro.class
+            }
+        });
     }
 }
